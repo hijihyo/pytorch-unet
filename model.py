76fbf51713f1422e2ae4c91777e21291fa58d4a3
Seunghyun Kim <shkim9533@korea.ac.kr>
2022-05-20 15:58:06 +0900

    Update model structure - use nn.Upsample instead of nn.ConvTranspose2d, make it more symmetric

--- model.py
+++ model.py
@@ -1,191 +1,203 @@
 """
-PyTorch U-Net model and modules for the model
+PyTorch U-Net and modules for it
 """
-from typing import List
+from typing import List, Tuple
 import torch
-from torch import nn
+import torch.nn as nn
+from torch import Tensor
+
+
+def conv_batch_relu(
+    in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int
+):
+    """Conv2d - BatchNorm2d - ReLU"""
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(),
+    )
 
 
 class UNetEncoderBlock(nn.Module):
-    """Constructs a encoder block of a U-Net model"""
+    """Constructs a block which an encoder of U-Net consists of"""
 
-    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, pool_size: int):
+    def __init__(
+        self, in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int
+    ) -> None:
         super(UNetEncoderBlock, self).__init__()
 
-        self.convs = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, kernel_size),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
-        )
-        self.maxpool = nn.MaxPool2d(pool_size)
+        conv_kwargs = {"kernel_size": kernel_size,
+                       "stride": stride, "padding": padding}
+        self.layer1 = conv_batch_relu(in_channels, out_channels, **conv_kwargs)
+        self.layer2 = conv_batch_relu(
+            out_channels, out_channels, **conv_kwargs)
+        self.downsample = nn.MaxPool2d(kernel_size=2)
 
-    def forward(self, input: torch.Tensor):
+    def forward(self, input: Tensor) -> Tuple[Tensor, Tensor]:
         """Args:
-            input: a `torch.Tensor`, (in_channels, width, height) or
-            (batch_size, in_channels, width, height)
+            input: torch.Tensor, (C, H, W) or (B, C, H, W)
 
         Returns:
-            output: a `torch.Tensor`, (out_channels, width', height') or
-            (batch_size, out_channels, width', height')
-            conv_output: a `torch.Tensor`, (out_channels, width',
-            height') or (batch_size, out_channels, width'', height'')
+            output: torch.Tensor, (C', H/2, W/2) or (B, C', H/2, W/2)
+            conv_output: torch.Tensor, (C', H, W) or (B, C', H, W)
         """
-        conv_output = self.convs(input)
-        output = self.maxpool(conv_output)
+        conv_output = self.layer1(input)
+        conv_output = self.layer2(conv_output)
+        output = self.downsample(conv_output)
         return output, conv_output
 
 
 class UNetDecoderBlock(nn.Module):
-    """Constructs a decoder block of a U-Net model"""
+    """Constructs a block which an decoder of U-Net consists of"""
 
-    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, pool_size: int):
+    def __init__(
+        self, in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int
+    ) -> None:
         super(UNetDecoderBlock, self).__init__()
 
-        self.upconv = nn.ConvTranspose2d(
-            in_channels, in_channels // 2, pool_size, stride=2)
-        self.convs = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, kernel_size),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
-        )
+        conv_kwargs = {"kernel_size": kernel_size,
+                       "stride": stride, "padding": padding}
+        # TODO: mode of `nn.Upsample`?
+        self.upsample = nn.Upsample(scale_factor=2, mode="bilinear")
+        # TODO: i've changed the structure
+        self.layer1 = conv_batch_relu(2 * in_channels,
+                                      in_channels, **conv_kwargs)
+        self.layer2 = conv_batch_relu(
+            in_channels, out_channels, **conv_kwargs)
 
-    def forward(self, input: torch.Tensor, ec_output: torch.Tensor):
+    def forward(self, input: Tensor, conv_output: Tensor) -> Tensor:
         """Args:
-            input: a `torch.Tensor`, (in_channels, width, height) or
-            (batch_size, in_channels, width, height)
-            ec_output: an output `torch.Tensor` from the same-level of
-            the encoder, (in_channels // 2, width', height') or
-            (batch_size, in_channels // 2, width', height')
+            input: torch.Tensor, (C, H, W) or (B, C, H, W)
+            conv_output: torch.Tensor, (C, H*2, W*2) or (B, C, H*2, W*2)
 
         Returns:
-            output: a `torch.Tensor`, (out_channels, width', height') or
-            (batch_size, out_channels, width', height')
+            output: torch.Tensor, (C', H*2, W*2) or (B, C', H*2, W*2)
         """
-        is_batched = input.dim() == 4
-        if not is_batched:
-            input = input.unsqueeze(0)
-            ec_output = ec_output.unsqueeze(0)
-        output = self.upconv(input)
-        width_diff = ec_output.size(2) - output.size(2)
-        height_diff = ec_output.size(3) - output.size(3)
-        eo_cropped = ec_output[:, :,
-                               width_diff//2:-width_diff//2,
-                               height_diff//2:-height_diff//2
-                               ]
-        output = torch.concat((output, eo_cropped), dim=1)
-        output = self.convs(output)
-        if not is_batched:
-            output = output.squeeze(0)
+        upsampled = self.upsample(input)  # (C, H*2, W*2)
+        combined = torch.cat((upsampled, conv_output),
+                             dim=input.dim()-3)  # (C*2, H*2, W*2)
+        output = self.layer1(combined)
+        output = self.layer2(output)
         return output
 
 
 class UNetEncoder(nn.Module):
-    """Constructs a encoder of a U-Net model"""
+    """Constructs an encoder of U-Net"""
 
     def __init__(
-        self, in_channels: int, blocks: List[int], out_channels: int,
-        kernel_size: int, pool_size: int, dropout: float = 0.
-    ):
+        self, in_channels: int, channels: int, kernel_size: int,
+        stride: int, padding: int, dropout: float = 0.
+    ) -> None:
         super(UNetEncoder, self).__init__()
+        self.in_channels = in_channels
+        self.channels = channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dropout = None
 
-        self.blocks = nn.ModuleList(
-            [UNetEncoderBlock(in_channels if i == 0 else blocks[i - 1],
-                              blocks[i], kernel_size, pool_size) for i, _ in enumerate(blocks)]
-        )
+        conv_kwargs = {"kernel_size": kernel_size,
+                       "stride": stride, "padding": padding}
+        self.blocks = nn.ModuleList([
+            UNetEncoderBlock(
+                in_channels if i == 0 else channels[i - 1],
+                channels[i],
+                **conv_kwargs
+            )
+            for i in range(len(channels) - 1)
+        ])
         if dropout > 0:
             self.dropout = nn.Dropout(dropout)
-        self.output_block = nn.Sequential(
-            nn.Conv2d(blocks[-1], out_channels, kernel_size),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
-        )
+        self.block_output = conv_batch_relu(
+            channels[-2], channels[-1], **conv_kwargs)
 
-    def forward(self, input: torch.Tensor):
+    def forward(self, input: Tensor) -> Tuple[Tensor, Tensor]:
         """Args:
-            input: a `torch.Tensor`, (in_channels, width, height) or
-            (batch_size, in_channels, width, height)
+            input: torch.Tensor, (C, H, W) or (B, C, H, W)
 
         Returns:
-            output: a `torch.Tensor`, (out_channels, width', height') or
-            (batch_size, out_channels, width, height)
-            block_output: a list of `torch.Tensor`s (length: num_blocks)
+            output: torch.Tensor, (C', H', W') or (B, C', H', W')
+            conv_output_list: a list of torch.Tensor
         """
         output = input
-        block_output = []
+        conv_output_list = []
         for i, _ in enumerate(self.blocks):
             output, conv_output = self.blocks[i](output)
-            block_output.append(conv_output)
+            conv_output_list.append(conv_output)
         if self.dropout is not None:
-            output = self.dropout(output)  # 원래 위치는 아님
-        output = self.output_block(output)
-        return output, block_output
+            output = self.dropout(output)
+        output = self.block_output(output)
+        return output, conv_output_list
 
 
 class UNetDecoder(nn.Module):
-    """Constructs a decoder of a U-Net model"""
+    """Constructs an decoder of U-Net"""
 
     def __init__(
-        self, in_channels: int, blocks: List[int], out_channels: int,
-        kernel_size: int, pool_size: int, dropout: float = 0.
-    ):
+        self, channels: int, num_classes: int, kernel_size: int,
+        stride: int, padding: int, dropout: float = 0.
+    ) -> None:
         super(UNetDecoder, self).__init__()
+        self.channels = channels
+        self.num_classes = num_classes
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dropout = None
 
-        self.input_block = nn.Sequential(
-            nn.Conv2d(in_channels, in_channels, kernel_size),
-            nn.BatchNorm2d(in_channels),
-            nn.ReLU(),
-        )
+        conv_kwargs = {"kernel_size": kernel_size,
+                       "stride": stride, "padding": padding}
+        self.block_input = conv_batch_relu(
+            channels[0], channels[1], **conv_kwargs)
         if dropout > 0:
             self.dropout = nn.Dropout(dropout)
-        self.blocks = nn.ModuleList(
-            [UNetDecoderBlock(in_channels if i == 0 else blocks[i - 1],
-                              blocks[i], kernel_size, pool_size) for i, _ in enumerate(blocks)]
-        )
-        self.output_block = nn.Conv2d(blocks[-1], out_channels, kernel_size=1)
+        self.blocks = nn.ModuleList([
+            UNetDecoderBlock(
+                channels[i],
+                num_classes if i + 1 == len(channels) else channels[i + 1],
+                **conv_kwargs
+            )
+            for i in range(1, len(channels))
+        ])
 
-    def forward(self, input: torch.Tensor, ec_output: List[torch.Tensor]):
+    def forward(self, input: Tensor, conv_output_list: List[Tensor]) -> Tensor:
         """Args:
-            input: a `torch.Tensor`, (in_channels, width, height) or
-            (batch_size, in_channels, width, height)
-            ec_output: a list of `torch.Tensor`s (length: num_blocks)
+            input: torch.Tensor, (C, H, W) or (B, C, H, W)
+            conv_output_list: a list of torch.Tensor
+
+        Returns:
+            output: torch.Tensor, (C', H', W') or (B, C', H', W')
         """
-        output = self.input_block(input)
+        output = self.block_input(input)
         if self.dropout is not None:
             output = self.dropout(output)
         for i, _ in enumerate(self.blocks):
-            output = self.blocks[i](output, ec_output[- i - 1])
-        output = self.output_block(output)
+            output = self.blocks[i](output, conv_output_list[i])
         return output
 
 
 class UNet(nn.Module):
-    """Constructs a U-Net model"""
+    """Constructs U-Net"""
 
-    def __init__(self, channels: List[int], num_classes: int, dropout: float = 0.):
+    def __init__(self, in_channels: int, num_classes: int, dropout: float = 0.) -> None:
         super(UNet, self).__init__()
         self.kernel_size = 3
-        self.pool_size = 2
+        self.stride = 1
+        self.padding = (self.kernel_size - self.stride) // 2
+        self.channels = [64, 128, 256, 512, 1024]
 
         self.encoder = UNetEncoder(
-            channels[0], channels[1:-1], channels[-1], self.kernel_size, self.pool_size, dropout)
+            in_channels, self.channels, self.kernel_size, self.stride, self.padding, dropout)
         self.decoder = UNetDecoder(
-            channels[-1], channels[-2:0:-1], num_classes, self.kernel_size, self.pool_size, dropout)
+            self.channels[::-1], num_classes, self.kernel_size, self.stride, self.padding, dropout)
 
-    def forward(self, input: torch.Tensor):
+    def forward(self, input: Tensor) -> Tensor:
         """Args:
-            input: a `torch.Tensor`, (in_channels, width, height) or
-            (batch_size, in_channels, width, height)
+            input: torch.Tensor, (C, H, W) or (B, C, H, W)
 
         Returns:
-            output: a `torch.Tensor`, (out_channels, width', height') or
-            (batch_size, out_channels, width, height)
+            output: torch.Tensor, (C', H, W) or (B, C', H, W)
         """
-        output, block_output = self.encoder(input)
-        output = self.decoder(output, block_output)
+        output, conv_output_list = self.encoder(input)
+        output = self.decoder(output, conv_output_list[::-1])
         return output
