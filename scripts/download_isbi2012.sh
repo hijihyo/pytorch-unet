8e48e51d0076de26aa119c467e12590e0f1207ca
Seunghyun Kim <shkim9533@korea.ac.kr>
2022-05-20 12:14:19 +0900

    Delete isbi2012 shell script file (i'll unuse it)

--- scripts/download_isbi2012.sh
+++
@@ -1,6 +0,0 @@
-# 프로젝트 폴더 최상단에서 실행할 것
-
-# Download ISBI 2012 EM segmentation challenge dataset
-git clone https://github.com/hoangp/isbi-datasets.git .data/isbi-datasets
-mv .data/isbi-datasets/data .data/ISBI2012
-rm -r .data/isbi-datasets
